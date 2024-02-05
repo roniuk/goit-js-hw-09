72caf8c0bb54b6edc76d7449cd200478fc51493e
Volodymyr <roniuk@ukr.net>
2024-02-05 16:49:48 +0200

    kjkjk

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -3,7 +3,7 @@ import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 const images = [
-     {
+  {
     preview:
       "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
     original:
@@ -11,52 +11,52 @@ const images = [
     description: "Hokkaido Flower",
   },
   {
-                preview: "src/img/koffe7.1.png",
-                original: "./goit-js-hw-09/images/schema1.4.png",
-                description: "schema",
-            },
+    preview: "src/img/koffe7.1.png",
+    original: "./goit-js-hw-09/images/schema1.4.png",
+    description: "schema",
+  },
 
-            {
-                preview: "/goit-js-hw-09/images/lampa2.1.png",
-                original: "/images/lampa2.4.png",
-                description: "lampa",
-            },
+  {
+    preview: "/goit-js-hw-09/images/lampa2.1.png",
+    original: "/images/lampa2.4.png",
+    description: "lampa",
+  },
 
-            {
-                preview: "/mobile3.1.png",
-                original: "/images/mobile3.4.png",
-                description: "mobile",
-            },
-            {
-                preview: "/images/mitting_room4.1.png",
-                original: "/images/mitting_room4.4.png",
-                description: "mitting-room",
-            },
-            {
-                preview: "/images/tennis_room5.1.png",
-                original: "/images/tennis__room5.4.png",
-                description: "tennis",
-            },
-            {
-                preview: "/images/work_room6.1.png",
-                original: "/images/work_room6.4.png",
-                description: "workroom",
-            },
-            {
-                preview: "/images/koffe7.1.png",
-                original: "/images/koffe7.4.png",
-                description: "coffe",
-            },
-            {
-                preview: "/images/worker8.1.png",
-                original: "/images/worker8.4.png",
-                description: "worker",
-            },
-            {
-                preview: "/images/lesson9.1.png",
-                original: "/images/lesson9.4.png",
-                description: "lesson",
-            },
+  {
+    preview: "./mobile3.1.png",
+    original: "/images/mobile3.4.png",
+    description: "mobile",
+  },
+  {
+    preview: "/images/mitting_room4.1.png",
+    original: "/images/mitting_room4.4.png",
+    description: "mitting-room",
+  },
+  {
+    preview: "/images/tennis_room5.1.png",
+    original: "/images/tennis__room5.4.png",
+    description: "tennis",
+  },
+  {
+    preview: "/images/work_room6.1.png",
+    original: "/images/work_room6.4.png",
+    description: "workroom",
+  },
+  {
+    preview: "/images/koffe7.1.png",
+    original: "/images/koffe7.4.png",
+    description: "coffe",
+  },
+  {
+    preview: "/images/worker8.1.png",
+    original: "/images/worker8.4.png",
+    description: "worker",
+  },
+  {
+    preview: "/images/lesson9.1.png",
+    original: "/images/lesson9.4.png",
+    description: "lesson",
+  },
 ];
 
 const gallery = document.querySelector('.gallery');
@@ -81,7 +81,7 @@ generateGallery(images);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
-  captionsPosition: 'bottom', 
-  captionsDelay: 250, 
+  captionsPosition: 'bottom',
+  captionsDelay: 250,
 
 });
